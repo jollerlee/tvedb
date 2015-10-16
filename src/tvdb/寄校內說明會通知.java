2fb42d602f92ab49d611b4d9dac658429b9731db
jollerlee <joller.lee@gmail.com>
2015-10-16 16:43:37 +0800

    extract meeting date and room as constant

--- src/tvdb/寄校內說明會通知.java
+++ src/tvdb/寄校內說明會通知.java
@@ -26,11 +26,13 @@ import org.apache.commons.lang3.tuple.Pair;
 
 public class 寄校內說明會通知 {
 
+	private static final String 舉辦地點 = "圖資五樓電腦教室二";
+	private static final String 舉辦日期 = "9/10(四)2:30pm";
+
 	public static void main(String[] args) throws IOException {
 		Map<String, List<InternetAddress>> unitEmails = new HashMap<>();
 
 		Utils.obtain單位email(unitEmails);
-		Utils.COMPRESSED_DIR.mkdir();
 
 		Map<InternetAddress, List<String>> emails = unitEmails.entrySet().stream()
 				.flatMap(e -> e.getValue().stream().map(email -> Pair.of(email, e.getKey())))
@@ -61,7 +63,7 @@ public class 寄校內說明會通知 {
 
 			// fill message
 			messageBodyPart.setText("您好，\n\n" + "技專資料庫填報作業已經開始，\n" + 
-					"資訊組將於9/10(四)於圖資五樓電腦教室二舉辦校內說明會，" + 
+					"資訊組將於 "+舉辦日期+" 於"+舉辦地點+"舉辦校內說明會，" + 
 					"請您務必撥冗出席。\n" + 
 					"依據資訊組的記錄，您負責的單位是" + units + "，\n" + 
 					"如有錯誤，請與資訊組聯絡更正，謝謝!\n\n" + "--\n" + "臺南護專/資訊組 李仁豪 (分機 232)\n",
